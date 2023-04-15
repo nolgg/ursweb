33c3b2bdb503337e3c36307579cc30ea83180c78
Beam Nathanon <86812729+nathanon11@users.noreply.github.com>
2023-04-15 15:10:45 +0700

    .

--- src/component/PATIENT/homept.js
+++ src/component/PATIENT/homept.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react';
 import { FiCode, FiMenu, FiX } from "react-icons/fi";
 import './Headerpt.css';
 import sr from './search.png';
+import mp from './25.png';
 import { Link } from 'react-router-dom';
 
 // let homeData = {
@@ -30,8 +31,13 @@ const closeMobileMenu = () => setClick(false);
                   <button className="homept-btn2" >
                   <Link  to="https://www.google.com/maps/search/%E0%B9%82%E0%B8%A3%E0%B8%87%E0%B8%9E%E0%B8%A2%E0%B8%B2%E0%B8%9A%E0%B8%B2%E0%B8%A5%E0%B9%83%E0%B8%81%E0%B8%A5%E0%B9%89%E0%B8%89%E0%B8%B1%E0%B8%99/@18.7950341,98.9831411,14z/data=!3m1!4b1?hl=th"  >โรงพยาบาลที่ใกล้เคียง</Link>
                   <img src={sr} style={{ width:'60px',marginLeft:'60px'}}></img>
+                  <br></br>
+                  <Link  style={{ fontSize:'30px'}}to="https://www.google.com/maps/search/%E0%B9%82%E0%B8%A3%E0%B8%87%E0%B8%9E%E0%B8%A2%E0%B8%B2%E0%B8%9A%E0%B8%B2%E0%B8%A5%E0%B9%83%E0%B8%81%E0%B8%A5%E0%B9%89%E0%B8%89%E0%B8%B1%E0%B8%99/@18.7950341,98.9831411,14z/data=!3m1!4b1?hl=th"  >Click Me</Link>
+                 
                   
                   </button>
+                  <img src={mp} style={{marginLeft:'260px',marginTop:'380px'}}/>
+                
                   </h3>
                  
            </div>
